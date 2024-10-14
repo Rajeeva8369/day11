006db04dd8c6813ea7f40fd7806843328e2a734c
Rajeeva8369 <rajeeva.ravichandiran@epitech.digital>
2024-10-14 10:49:16 +0200

    ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,8 +1,14 @@
 function checkFuel(fuel) {
-  return new Promise((resolve, reject) => {
-    if (fuel >= 9) {
-      resolve("Ready to launch");
-    } else {
-      reject("Please refill fuel");
-    }});
+    return new Promise((resolve, reject) => {
+        if (fuel >= 9) {
+            resolve("Ready to launch");
+        } else {
+            reject("Please refill fuel");
+        }});
 }
+checkFuel(10)
+    .then((message) => {
+        console.log(message);  })
+    .catch((error) => {
+        console.log(error);  
+    });
